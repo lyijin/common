c66b9a86f31e0ccd56af88c68e9092c67514ca28
Yi Jin Liew <yijin.liew@kaust.edu.sa>
2018-09-25 12:24:45 +1000

    slight modification to grant_pymissions.sh

--- grant_pymissions.sh
+++ grant_pymissions.sh
@@ -2,8 +2,10 @@
 
 chmod 755 ~/kaust/common/*.py
 chmod 755 ~/kaust/common/*.sh
+chmod 755 ~/kaust/common/*.R
 chmod 755 *.py
 chmod 755 *.sh
+chmod 755 *.R
 
-echo Granted 755 permissions to all \*.py and \*.sh in ~/kaust/common!
-echo Also, granted 755 permissions to *.py *.sh in this directory!
+echo Granted 755 permissions to all \*.py, \*.sh and \*.R in ~/kaust/common!
+echo Also, granted 755 permissions to *.py *.sh *.R in this directory!
