462eb5d383b417f08a13bbfb6d6eba0dd3ed0a72
Yi Jin Liew <yijin.liew@kaust.edu.sa>
2020-11-30 16:53:48 +1100

    fixed tally_ATCGN

--- tally_ACGTN.py
+++ tally_ACGTN.py
@@ -10,6 +10,7 @@ N = any degenerate base (R/Y/...), not just "N".
 """
 import argparse
 import collections
+from pathlib import Path
 
 import parse_fasta
 
@@ -18,12 +19,10 @@ Script to tally the proportion of A/C/G/T/N bases in a FASTA/FASTQ file.
 Supports opening gzipped files with the --gzip / -g flag.""")
 
 parser.add_argument('fasta_files', metavar='fasta_files',
-                    type=argparse.FileType('r'), nargs='+',
+                    type=Path, nargs='+',
                     help='FASTA files that will be tallied.')
 parser.add_argument('--fastq', action='store_true', default=False,
                     help='Tallied files are FASTQ, not FASTA.')
-parser.add_argument('--gzip', '-g', action='store_true', default=False,
-                    help='Tallied files are gzip-compressed.')
 
 args = parser.parse_args()
 
@@ -33,7 +32,6 @@ print ('File', 'A', 'C', 'G', 'T', 'N', 'ACGT', 'ACGTN', 'GC%', sep='\t')
 for f in args.fasta_files:
     base_composition = collections.Counter()
     seqs = parse_fasta.get_all_sequences(f, 'fastq' if args.fastq else 'fasta',
-                                         gzip_compressed=args.gzip,
                                          sequences_only=True)
     
     for s in seqs:
